08975e9ad2366fa709cc6e8ba71478f49bcdcd48
시혁 박 <exon.indiv@gmail.com>
2020-08-27 22:17:33 +0900

    black

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 function inject(path, type) {
-  if(type == "file") {
-    chrome.tabs.insertCSS({file: path, runAt: "document_end"});
-  } else {
-    chrome.tabs.insertCSS({code: path, runAt: "document_end"});
-  }
+    if(type == "file") {
+        chrome.tabs.insertCSS({file: path, runAt: "document_end"});
+    } else {
+        chrome.tabs.insertCSS({code: path, runAt: "document_end"});
+    }
 }
 
 function load(item) {
-  const loadedDark = localStorage.getItem(item);
-  return loadedDark
+    const loadedDark = localStorage.getItem(item);
+    return loadedDark
 }
 
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (load("dark") == "On"){
-    inject("./theme/black.css", "file");
-  }
+    if (load("dark") == "On"){
+        inject("./theme/black.css", "file");
+    }
 
 })
