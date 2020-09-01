649ee195d062f3e6db0b90c20ffc8e2b38e3c88a
시혁 박 <exon.indiv@gmail.com>
2020-09-01 20:39:46 +0900

    v1.2

--- advertising.js
+++ advertising.js
@@ -2,6 +2,6 @@
 try {
     let advertising = document.querySelector('body > div.wrapper > div.container.content > div.text-center.no-print > a > img.visible-xs-block')
     advertising.parentElement.removeChild(advertising)
-} catch{
-
+} catch {
+    
 }
