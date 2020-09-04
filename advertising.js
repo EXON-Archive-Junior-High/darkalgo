5e9e23242cbe1458d4cfb65cefbb22901b8f80da
시혁 박 <exon.indiv@gmail.com>
2020-09-04 13:25:41 +0900

    광고 삭제

--- advertising.js
+++ advertising.js
@@ -1,3 +1,7 @@
 /* 광고 삭제 */
-let advertising = document.querySelector('body > div.wrapper > div.container.content > div.text-center.no-print > a > img.visible-md-block')
-advertising.parentElement.removeChild(advertising)
+try {
+    let advertising = document.querySelector('body > div.wrapper > div.container.content > div.text-center.no-print > a')
+    advertising.parentElement.removeChild(advertising)
+} catch {
+    console.log('광고 삭제 실패')
+}
