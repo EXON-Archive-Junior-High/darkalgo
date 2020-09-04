61227a7caee5343f19f73d9fea00b36011ed392f
시혁 박 <exon.indiv@gmail.com>
2020-09-04 13:22:55 +0900

    advertising

--- advertising.js
+++ advertising.js
@@ -1,7 +1,3 @@
 /* 광고 삭제 */
-try {
-    let advertising = document.querySelector('body > div.wrapper > div.container.content > div.text-center.no-print > a > img.visible-xs-block')
-    advertising.parentElement.removeChild(advertising)
-} catch {
-    
-}
+let advertising = document.querySelector('body > div.wrapper > div.container.content > div.text-center.no-print > a > img.visible-md-block')
+advertising.parentElement.removeChild(advertising)
